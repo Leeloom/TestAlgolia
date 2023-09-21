a73a7b8ec2789ec6cab4b85661876027cbad74c4
Manon <130955857+Leeloom@users.noreply.github.com>
2023-09-21 18:18:06 +0200

    searchbar propre

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,4 @@
 // Import and register all your controllers from the importmap under controllers/*
-
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
@@ -10,18 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
 // Initialisez le client Algolia avec votre ApplicationID et votre Search-Only-API-Key
-var client = algoliasearch('PDO8SRTL3V', 'ea191d93879875e591d1bbdde49e5197');
-
-// Initialisez l'index sur lequel vous souhaitez effectuer des recherches
-var index = client.initIndex('Character');
-
-// Effectuez une recherche avec des options (par exemple, 10 résultats par page, page 0)
-index.search('something', { hitsPerPage: 10, page: 0 })
-  .then(function searchDone(content) {
-    // Traitement des résultats de la recherche
-    console.log(content);
-  })
-  .catch(function searchFailure(err) {
-    // Gestion des erreurs en cas d'échec de la recherche
-    console.error(err);
-  });
